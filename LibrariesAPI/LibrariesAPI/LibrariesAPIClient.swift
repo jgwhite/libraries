daf32ad276046ee18f244eb5fd4772cfa370933a
Jamie White <jamie@jgwhite.co.uk>
2016-01-15 15:08:04 +0000

    Complete test suite

--- LibrariesAPI/LibrariesAPI/LibrariesAPIClient.swift
+++ LibrariesAPI/LibrariesAPI/LibrariesAPIClient.swift
@@ -11,48 +11,49 @@ import Foundation
 public typealias LibrariesAPICallback = NSDictionary? -> Void
 
 public struct LibrariesAPIClient {
+
     public let apiKey: String
-    
+
     public init(apiKey: String) {
         self.apiKey = apiKey
     }
-    
+
     public func project(platform: String, _ name: String, then: NSDictionary? -> Void) {
         fetch("\(platform)/\(name)") { then($0 as? NSDictionary) }
     }
-    
-    public func project_dependencies(platform: String, _ name: String, _ version: String = "latest", then: NSDictionary? -> Void) {
+
+    public func projectDependencies(platform: String, _ name: String, _ version: String = "latest", then: NSDictionary? -> Void) {
         fetch("\(platform)/\(name)/\(version)/dependencies") { then($0 as? NSDictionary) }
     }
-    
-    public func project_dependents(platform: String, _ name: String, then: NSArray -> Void) {
+
+    public func projectDependents(platform: String, _ name: String, then: NSArray -> Void) {
         fetch("\(platform)/\(name)/dependents") { then($0 as! NSArray) }
     }
-    
-    public func project_dependent_repositories(platform: String, _ name: String, then: NSArray -> Void) {
+
+    public func projectDependentRepositories(platform: String, _ name: String, then: NSArray -> Void) {
         fetch("\(platform)/\(name)/dependent_repositories") { then($0 as! NSArray) }
     }
-    
+
     public func search(q: String, then: NSArray -> Void) {
         fetch("search", ["q": q]) { then($0 as! NSArray) }
     }
-    
+
     public func github(owner: String, _ name: String, then: NSDictionary? -> Void) {
         fetch("github/\(owner)/\(name)") { then($0 as? NSDictionary) }
     }
-    
-    public func github_dependencies(owner: String, _ name: String, then: NSDictionary? -> Void) {
+
+    public func githubDependencies(owner: String, _ name: String, then: NSDictionary? -> Void) {
         fetch("github/\(owner)/\(name)/dependencies") { then($0 as? NSDictionary) }
     }
-    
-    public func github_projects(owner: String, _ name: String, then: NSArray -> Void) {
+
+    public func githubProjects(owner: String, _ name: String, then: NSArray -> Void) {
         fetch("github/\(owner)/\(name)/projects") { then($0 as! NSArray) }
     }
-    
+
     func fetch(resource: String, _ params: [String: String] = [String: String](), then: AnyObject? -> Void) {
         let session = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
-        let query = params.map { "\($0)=\($1)" }.joinWithSeparator("&")
-        let url = NSURL(string: "https://libraries.io/api/\(resource)?api_key=\(apiKey)&\(query)")!
+        let query = (["api_key=\(apiKey)"] + params.map { "\($0)=\($1)" }).joinWithSeparator("&")
+        let url = NSURL(string: "https://libraries.io/api/\(resource)?\(query)")!
         let task = session.dataTaskWithURL(url) { data, response, error in
             if let data = data, dict = self.parseJSON(data) {
                 then(dict)
@@ -62,7 +63,7 @@ public struct LibrariesAPIClient {
         }
         task.resume()
     }
-    
+
     func parseJSON(data: NSData) -> AnyObject? {
         do {
             return try NSJSONSerialization.JSONObjectWithData(data, options: .MutableContainers)
@@ -70,4 +71,5 @@ public struct LibrariesAPIClient {
             return nil
         }
     }
+
 }
--- LibrariesAPI/LibrariesAPITests/LibrariesAPITests.swift
+++ LibrariesAPI/LibrariesAPITests/LibrariesAPITests.swift
@@ -12,21 +12,201 @@ import LibrariesAPI
 
 class LibrariesAPITests: XCTestCase {
 
-    func testProject() {
-        let expectation = expectationWithDescription("client.project")
-        let client = LibrariesAPIClient(apiKey: "test-api-key")
-        let body = [ "name": "grunt", "rank": 24 ]
-        
-        stub(http(.GET, uri: "/api/npm/grunt"), builder: json(body))
-        
+    var client: LibrariesAPIClient!
+
+    override func setUp() {
+        client = LibrariesAPIClient(apiKey: "test-api-key")
+    }
+
+    func testProjectSuccess() {
+        let expectation = expectationWithDescription("testProjectSuccess")
+
+        stub(
+            http(.GET, "https://libraries.io/api/npm/grunt?api_key=test-api-key"),
+            builder: json(["name":  "grunt"])
+        )
+
         client.project("npm", "grunt") { project in
-            XCTAssert(project?["name"] as? String == "grunt")
-            XCTAssert(project?["rank"] as? Int == 24)
+            XCTAssertEqual(project?["name"] as? String, "grunt")
+
             expectation.fulfill()
         }
-        
-        waitForExpectationsWithTimeout(5) { error in
-            print("Timeout error: \(error)")
+
+        waitForExpectationsWithTimeout(5) { print("Timeout error: \($0)") }
+    }
+
+    func testProjectNotFound() {
+        let expectation = expectationWithDescription("testProjectNotFound")
+
+        stub(
+            http(.GET, "https://libraries.io/api/npm/grunt?api_key=test-api-key"),
+            builder: json([], status: 404)
+        )
+
+        client.project("npm", "grunt") { project in
+            XCTAssertNil(project)
+
+            expectation.fulfill()
+        }
+
+        waitForExpectationsWithTimeout(5) { print("Timeout error: \($0)") }
+    }
+
+    func testProjectDependenciesSuccess() {
+        let expectation = expectationWithDescription("testProjectDependenciesSuccess")
+
+        stub(
+            http(.GET, "https://libraries.io/api/npm/grunt/latest/dependencies?api_key=test-api-key"),
+            builder: json(["name": "grunt"])
+        )
+
+        client.projectDependencies("npm", "grunt") { project in
+            XCTAssertEqual(project?["name"] as? String, "grunt")
+
+            expectation.fulfill()
+        }
+
+        waitForExpectationsWithTimeout(5) { print("Timeout error: \($0)") }
+    }
+
+    func testProjectDependents() {
+        let expectation = expectationWithDescription("testProjectDependents")
+
+        stub(
+            http(.GET, "https://libraries.io/api/npm/grunt/dependents?api_key=test-api-key"),
+            builder: json([["name": "some-other-project"]])
+        )
+
+        client.projectDependents("npm", "grunt") { dependents in
+            let project = dependents[0] as? [String: AnyObject]
+
+            XCTAssertEqual(project?["name"] as? String, "some-other-project")
+
+            expectation.fulfill()
+        }
+
+        waitForExpectationsWithTimeout(5) { print("Timeout error: \($0)") }
+    }
+
+    func testProjectDependentsNotFound() {
+        let expectation = expectationWithDescription("testProjectDependentsNotFound")
+
+        stub(
+            http(.GET, "https://libraries.io/api/npm/grunt/dependents?api_key=test-api-key"),
+            builder: json([], status: 404)
+        )
+
+        client.projectDependents("npm", "grunt") { dependents in
+            XCTAssertEqual(dependents.count, 0)
+
+            expectation.fulfill()
+        }
+
+        waitForExpectationsWithTimeout(5) { print("Timeout error: \($0)") }
+    }
+
+    func testProjectDependentRepositories() {
+        let expectation = expectationWithDescription("testProjectDependentRepositories")
+
+        stub(
+            http(.GET, "https://libraries.io/api/npm/grunt/dependent_repositories?api_key=test-api-key"),
+            builder: json([["name": "some-other-project"]])
+        )
+
+        client.projectDependentRepositories("npm", "grunt") { dependents in
+            let project = dependents[0] as? [String: AnyObject]
+
+            XCTAssertEqual(project?["name"] as? String, "some-other-project")
+
+            expectation.fulfill()
+        }
+
+        waitForExpectationsWithTimeout(5) { print("Timeout error: \($0)") }
+    }
+
+    func testSearch() {
+        let expectation = expectationWithDescription("testSearch")
+
+        stub(
+            http(.GET, "https://libraries.io/api/search?api_key=test-api-key&q=foo"),
+            builder: json([["name": "foo"]])
+        )
+
+        client.search("foo") { dependents in
+            let dependent = dependents[0] as? [String: AnyObject]
+
+            XCTAssertEqual(dependent?["name"] as? String, "foo")
+
+            expectation.fulfill()
+        }
+
+        waitForExpectationsWithTimeout(5) { print("Timeout error: \($0)") }
+    }
+
+    func testGithub() {
+        let expectation = expectationWithDescription("testGithub")
+
+        stub(
+            http(.GET, "https://libraries.io/api/github/gruntjs/grunt?api_key=test-api-key"),
+            builder: json(["full_name": "gruntjs/grunt"])
+        )
+
+        client.github("gruntjs", "grunt") { project in
+            XCTAssertEqual(project?["full_name"] as? String, "gruntjs/grunt")
+
+            expectation.fulfill()
+        }
+
+        waitForExpectationsWithTimeout(5) { print("Timeout error: \($0)") }
+    }
+
+    func testGithubDependencies() {
+        let expectation = expectationWithDescription("testGithubDependencies")
+
+        stub(
+            http(.GET, "https://libraries.io/api/github/gruntjs/grunt/dependencies?api_key=test-api-key"),
+            builder: json(["full_name": "gruntjs/grunt"])
+        )
+
+        client.githubDependencies("gruntjs", "grunt") { project in
+            XCTAssertEqual(project?["full_name"] as? String, "gruntjs/grunt")
+
+            expectation.fulfill()
+        }
+
+        waitForExpectationsWithTimeout(5) { print("Timeout error: \($0)") }
+    }
+
+    func testGithubProjects() {
+        let expectation = expectationWithDescription("testGithubProjects")
+
+        stub(
+            http(.GET, "https://libraries.io/api/github/gruntjs/grunt/projects?api_key=test-api-key"),
+            builder: json([["full_name": "gruntjs/grunt"]])
+        )
+
+        client.githubProjects("gruntjs", "grunt") { projects in
+            let project = projects[0] as? [String: AnyObject]
+
+            XCTAssertEqual(project?["full_name"] as? String, "gruntjs/grunt")
+
+            expectation.fulfill()
+        }
+
+        waitForExpectationsWithTimeout(5) { print("Timeout error: \($0)") }
+    }
+
+
+    func http(method: HTTPMethod, _ urlString: String) -> NSURLRequest -> Bool {
+        let url = NSURL(string: urlString)!
+
+        return { (request: NSURLRequest) in
+            XCTAssertEqual(request.HTTPMethod, method.description)
+            XCTAssertEqual(request.URL?.scheme, url.scheme)
+            XCTAssertEqual(request.URL?.host, url.host)
+            XCTAssertEqual(request.URL?.path, url.path)
+            XCTAssertEqual(request.URL?.query, url.query)
+            return true
         }
     }
 
